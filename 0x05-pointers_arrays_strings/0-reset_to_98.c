bb2bbcf08e97fc7b91bc4c15e7117592c387492c
Ahmed Hatem <54455354+AhmedHatemS@users.noreply.github.com>
2023-02-27 20:52:09 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
-  * reset_to_98 - set a pointer to 98.
-  * @n: pointer to change
-  */
+* reset_to_98 - set a pointer to 98.
+* @n: pointer to change
+*/
 void reset_to_98(int *n)
-{*n = 98;}
+{
+*n = 98;
+}
