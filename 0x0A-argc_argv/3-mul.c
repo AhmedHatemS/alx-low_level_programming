2949e0b33d50b41c5b431398658b572ac73b28b4
AhmedHatemS <dev.ahmedhatem@gmail.com>
2023-03-10 10:05:53 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,6 +7,7 @@
  * @argv: argument count
  * Return: Always 0
  */
+
 int main(int argc, char *argv[])
 {
 int n1 = 0, n2 = 0;
@@ -15,7 +16,7 @@ if (argc == 3)
 {
 n1 = atoi(argv[1]);
 n2 = atoi(argv[2]);
-printf("%d\n", n1 * n2);
+printf("%d\n", n1 *n2);
 }
 else
 {
